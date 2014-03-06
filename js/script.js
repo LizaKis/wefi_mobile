57659b99baa8ae2c417a0a93c8d2817c389b0ee0
Anastasiya Kozaeva <kozaeva_n@progforce.com>
2014-03-06 16:12:55 +0400

    team

---
+++ js/script.js
@@ -0,0 +1,30 @@
+(function($) { 
+  $(document).ready(function() {
+
+    $('.view-management .views-row').each(function(i) {
+      $(this).find('.views-field-field-link').find('.field-content').prepend(
+      "<div class='full-biography'>Full Biography</div><div class='close-text'>Close</div>");
+    });
+
+    $('.full-biography').click(function() {
+      $(this).parents('.views-field-field-link').prev().find("#content-summary").css('display','none');
+      $(this).parents('.views-field-field-link').prev().find("#content-body").css('display','block');      
+      $(this).css('display','none');
+      $(this).parents('.views-row').addClass('views-row-open');      
+    });
+
+    $('.close-text').click(function() {
+      if ($(this).parents('.views-field-field-link').prev().find("#content-summary").length > 0) {
+        $(this).parents('.views-field-field-link').prev().find("#content-summary").css('display','block');
+        $(this).parents('.views-field-field-link').prev().find("#content-body").css('display','none');
+      }
+      else {
+        $(this).parents('.field-content').css('height','40px');
+      }      
+      $(this).parents('.views-field-field-link').prev().css('border-bottom','0px');
+      $(this).parents('.views-row').removeClass('views-row-open');
+      $(this).prev().css('display','block');
+    });
+
+  });
+})(jQuery);
